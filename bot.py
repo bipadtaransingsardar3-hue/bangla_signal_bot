8f0539a7ed4ffc371193b83776aaaf5614f0a1c7
bipadtaransingsardar3-hue <bipadtaransingsardar3@gmail.com>
2026-03-04 21:11:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,60 @@
 import telebot
 import os
+import requests
+import pandas as pd
+import time
 
 TOKEN = os.environ.get("BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
 CHAT_ID = "8178103030"
 
-bot.send_message(CHAT_ID, "🔥 Test Signal Working!")
-print("Message Sent")
+symbol = "BTCUSDT"
+interval = "15m"
+
+last_signal = None
+
+def get_data():
+    url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit=250"
+    data = requests.get(url).json()
+    df = pd.DataFrame(data)
+    df = df.iloc[:, :6]
+    df.columns = ["time","open","high","low","close","volume"]
+    df = df.astype(float)
+    return df
+
+def check_signal():
+    global last_signal
+    df = get_data()
+
+    df["EMA200"] = df["close"].ewm(span=200).mean()
+
+    last_close = df["close"].iloc[-1]
+    last_high = df["high"].iloc[-1]
+    last_low = df["low"].iloc[-1]
+
+    ema = df["EMA200"].iloc[-1]
+
+    highest_high = df["high"].iloc[-21:-1].max()
+    lowest_low = df["low"].iloc[-21:-1].min()
+
+    # BUY
+    if last_close > ema and last_high > highest_high:
+        if last_signal != "BUY":
+            last_signal = "BUY"
+            return "📈 BUY SIGNAL\nEMA200 Uptrend + Breakout"
+
+    # SELL
+    if last_close < ema and last_low < lowest_low:
+        if last_signal != "SELL":
+            last_signal = "SELL"
+            return "📉 SELL SIGNAL\nEMA200 Downtrend + Breakdown"
+
+    return None
+
+
+while True:
+    signal = check_signal()
+    if signal:
+        bot.send_message(CHAT_ID, f"BTCUSDT 15m\n{signal}")
+    time.sleep(60)
